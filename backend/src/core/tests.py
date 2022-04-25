e817a4476454287dc83eaface33a485762fb683f
Roger Naranjo <roger.naranjo-garcia@de.sasol.com>
2022-04-25 12:35:50 +0200

    adding test

--- backend/src/core/tests.py
+++ backend/src/core/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from datetime import datetime
 
-# Create your tests here.
+from django.contrib.auth.models import User
+import pandas as pd
+
+from django.urls import reverse
+from rest_framework import status
+
+
+class WorkflowTestCase(APITestCase):
+    @classmethod
+    def setUpClass(self):
+        super().setUpClass()
+        self.user = User.objects.create_user(
+            username='admin', email='admin@admin.com', password='top_secret')
+        self.client = APIClient()
+        self.client.login(username='admin', password='top_secret')
+        
+
+        
+
+    def test_get_quality_data(self):
+
+      
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(reverse('get_quality'))
+        # Check that we get data
+        self.assertGreater(pd.DataFrame(response.json()).shape[0], 0)
+        
--- backend/src/core/urls.py
+++ backend/src/core/urls.py
@@ -10,12 +10,12 @@ from core.views import GetUpdateResView
 from core.views import RunTests
 
 urlpatterns = [
-    path('get_quality/', GetQualityView.as_view()),
-    path('get_price/', GetPriceView.as_view()),
-    path('get_inputs/', GetInputsView.as_view()),
-    path('get_inputs_editable/', GetInputsEditableView.as_view()),
-    path('get_planned/', GetPlannedView.as_view()),
-    path('get_res/', GetResView.as_view()),
-    path('update_all_res/', GetUpdateResView.as_view()),
+    path('get_quality/', GetQualityView.as_view(), name='get_quality'),
+    path('get_price/', GetPriceView.as_view(), name='get_price'),
+    path('get_inputs/', GetInputsView.as_view(), name='get_inputs'),
+    path('get_inputs_editable/', GetInputsEditableView.as_view(),   name='get_inputs_editable'),
+    path('get_planned/', GetPlannedView.as_view(), name='get_planned'),
+    path('get_res/', GetResView.as_view(), name='get_res'),
+    path('update_all_res/', GetUpdateResView.as_view(),   name='update_all_res'),
     path('test/', RunTests.as_view()),
 ]
